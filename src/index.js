0a0ab1fd7661c72b1591579ffc0ae240a9aaa636
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2021-09-17 08:37:43 +0530

    add firebase config info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD14dN09eIE9BpY_L8WbhX7R7T4vepWVJ0",
+  authDomain: "group-chat-43506.firebaseapp.com",
+  projectId: "group-chat-43506",
+  storageBucket: "group-chat-43506.appspot.com",
+  messagingSenderId: "820416837001",
+  appId: "1:820416837001:web:adf7d94b555a7bc8b51d58",
+  measurementId: "G-BFK2SE4JMT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
